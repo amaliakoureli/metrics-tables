fca3ffbaea4f7dcb42aacf8f80712e013edf616d
Amalia <akoureli@olympia-electronics.com>
2025-03-24 11:24:29 +0200

    rows 14 and 15 added with opening and focus features

---
+++ metrics-table/src/Row15.jsx
@@ -0,0 +1,131 @@
+import React, {useState} from "react";
+import "./Row4.css";
+
+
+function Row15({ row, 
+                index, 
+                handleEnterPress,
+                handleChange,
+                input1,
+                input2,
+                input3,}){
+    
+      
+return(
+<>
+
+<tr key={row.id} className="table-row" data-row={index === 0 ? "0" : undefined}>
+    <td className="table-cell">{row.id}</td>
+    <td className="table-cell">{row.text}</td>
+    <td className="table-cell">
+        <table>
+        <tbody>
+            <tr>
+            <td>
+                <input
+                    type="text"
+                    value={"1 ώρα : " + (input1?.replace("1 ώρα : ", "") ?? "")}
+                    onChange={(e) => {
+                      const inputValue = e.target.value;
+                      const prefix = "1 ώρα : ";
+                      
+                      if (inputValue.startsWith(prefix)) {
+                        handleChange(index, "input1", inputValue);
+                      } else {
+                        handleChange(index, "input1", prefix + inputValue);
+                      }
+                    }}
+                    onKeyDown={(e) => {
+                      const prefix = "1 ώρα : ";
+                      const cursorPos = e.target.selectionStart;
+
+                      if (
+                        (e.key === "Backspace" && cursorPos <= prefix.length) ||
+                        (e.key === "Delete" && cursorPos < prefix.length)
+                      ) {
+                        e.preventDefault();
+                      }
+                      handleEnterPress(e, index, true);
+                    }}
+                    className="input-style input1"
+                  />
+            </td>
+            </tr>
+            <tr>
+            <td>
+            <input
+                    type="text"
+                    value={"3 ώρες : " + (input2?.replace("3 ώρες : ", "") ?? "")}
+                    onChange={(e) => {
+                      const inputValue = e.target.value;
+                      const prefix = "3 ώρες : ";
+                      
+                      if (inputValue.startsWith(prefix)) {
+                        handleChange(index, "input2", inputValue);
+                      } else {
+                        handleChange(index, "input2", prefix + inputValue);
+                      }
+                    }}
+                    onKeyDown={(e) => {
+                      const prefix = "3 ώρες : ";
+                      const cursorPos = e.target.selectionStart;
+
+                      if (
+                        (e.key === "Backspace" && cursorPos <= prefix.length) ||
+                        (e.key === "Delete" && cursorPos < prefix.length)
+                      ) {
+                        e.preventDefault();
+                      }
+                      handleEnterPress(e, index, true);
+                    }}
+                    className="input-style input2"
+                  />
+            </td>
+            </tr>
+            <tr>
+                <td>
+                <input
+                    type="text"
+                    value={"8 ώρες : " + (input3?.replace("8 ώρες : ", "") ?? "")}
+                    onChange={(e) => {
+                      const inputValue = e.target.value;
+                      const prefix = "8 ώρες : ";
+                      
+                      if (inputValue.startsWith(prefix)) {
+                        handleChange(index, "input3", inputValue);
+                      } else {
+                        handleChange(index, "input3", prefix + inputValue);
+                      }
+                    }}
+                    onKeyDown={(e) => {
+                      const prefix = "8 ώρες : ";
+                      const cursorPos = e.target.selectionStart;
+
+                      if (
+                        (e.key === "Backspace" && cursorPos <= prefix.length) ||
+                        (e.key === "Delete" && cursorPos < prefix.length)
+                      ) {
+                        e.preventDefault();
+                      }
+                      handleEnterPress(e, index, true);
+                    }}
+                    className="input-style input3"
+                  />
+                </td>
+            </tr>
+        </tbody>
+        </table>
+     </td>
+</tr>
+    </>
+  );
+}
+
+
+
+export default Row15;
+
+
+
+
+
